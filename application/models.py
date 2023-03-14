4c60023b13737a80fc3f2a0c1db71f5e8b75c250
anant <95141712+anant-357@users.noreply.github.com>
2023-03-14 14:52:08 +0530

    Adding Users and Bookings Models

--- application/models.py
+++ application/models.py
@@ -17,3 +17,19 @@ class Shows(db.Model):
     Rating = db.Column(db.Integer)
     Tags = db.Column(db.String)
     TicketPrice = db.Column(db.Integer)
+
+
+class Users(db.Model):
+    __table__name = "USERS"
+    ID = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    UserName = db.Column(db.String, unique=True)
+    Password = db.Column(db.String)
+    Admin = db.Column(db.Integer)
+
+
+class Bookings(db.Model):
+    __table__name = "BOOKED"
+    ID = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    User_ID = db.Column(db.Integer, db.ForeignKey("USERS.ID"))
+    Show_ID = db.Column(db.Integer, db.ForeignKey("SHOWS.ID"))
+    
